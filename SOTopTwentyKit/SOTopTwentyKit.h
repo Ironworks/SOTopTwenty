ad7bb1fcf31c1a0d1ca2a1e7c6700b58c78a8b4e
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2020-02-13 00:21:35 +0000

    Initial Project Setup

---
+++ SOTopTwentyKit/SOTopTwentyKit.h
@@ -0,0 +1,19 @@
+//
+//  SOTopTwentyKit.h
+//  SOTopTwentyKit
+//
+//  Created by Trevor Doodes on 12/02/2020.
+//  Copyright © 2020 IronworksMediaLimited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SOTopTwentyKit.
+FOUNDATION_EXPORT double SOTopTwentyKitVersionNumber;
+
+//! Project version string for SOTopTwentyKit.
+FOUNDATION_EXPORT const unsigned char SOTopTwentyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOTopTwentyKit/PublicHeader.h>
+
+
